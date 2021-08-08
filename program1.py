b37784a5d09bf06a0359a9144d836cd8404f653a
sadasivuni durga <39164201+durgas4@users.noreply.github.com>
2021-08-08 15:52:46 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -2,16 +2,16 @@
 def factor(n):
     lst=[]
     for i in range(1,n+1):
-        if n%i==0:
+        if n%i==0:# evaluating the factors of n
             lst=lst+[i]
     return(lst)
 def isprime(o):
-    return(factor(o)==[1,o])          
+    return(factor(o)==[1,o])  # returns prime numbers with factors zero and itself
 
 def primeproduct(m):
     lists=[]
-    for p in range (2,m+1):
-        for q in range (2,p):
+    for p in range (2,m+1): # for all m
+        for q in range (2,p): # for all p with q<p
             if isprime(q):
                 if isprime(p):
                     if m==p*q:
